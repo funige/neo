4fa4d94a5f9966bcc2af00b9e022cc70763a54c8
funige <funigedev@gmail.com>
2018-01-29 05:38:02 +0900

    アプリ版の脆弱性対策
    Former-commit-id: 9b28508f77abdefbbb38b92b6438706e4bcd224b

--- neo/dist/PaintBBS-1.2.3.js
+++ neo/dist/PaintBBS-1.2.3.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var Neo = function() {};
 
-Neo.version = "1.2.3";
+Neo.version = "1.2.4";
 Neo.painter;
 Neo.fullScreen = false;
 Neo.uploaded = false;
