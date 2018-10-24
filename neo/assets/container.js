cdf2c90dc0f358a02b66920e97ac26578dcd34cb
funige <funigedev@gmail.com>
2018-10-25 07:24:44 +0900

    githubでsecurity alertが出るので対処
    Former-commit-id: 3f85306f2f28e42f164e572af3c57bca6a330a59

--- neo/assets/container.js
+++ neo/assets/container.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var Neo = function() {};
 
-Neo.version = "1.4.6";
+Neo.version = "1.4.7";
 Neo.painter;
 Neo.fullScreen = false;
 Neo.uploaded = false;
