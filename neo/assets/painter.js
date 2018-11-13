326e59b929984a7713db9dd8ab04130843977994
funige <funigedev@gmail.com>
2018-11-13 11:27:28 +0900

    v1.4.11
    Former-commit-id: fdcfd3db3bef3545e4b593f2138a966c1db41119

--- neo/assets/painter.js
+++ neo/assets/painter.js
@@ -2556,10 +2556,10 @@ Neo.Painter.prototype.onrewind = function() {
 Neo.Painter.prototype.onmark = function() {
     if (Neo.viewerBar) Neo.viewerBar.update();
     if (!this._actionMgr._pause) {
-        if (this._actionMgr._head < this._actionMgr._mark) {
-            this.onplay();
-        } else {
+        if (this._actionMgr._head > this._actionMgr._mark) {
             this.onrewind();
+        } else {
+            this.onplay();
         }
     }
 };
