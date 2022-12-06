b362a0d846ff7598cb99b290948aede68817481a
satopian <satopian@mia.eek.jp>
2022-12-06 21:10:29 +0900

    管理者による設定変の更が必要な403、404の時は、エラーメッセージにステータスを追加。回線が切断されている時にもエラーメッセージを追加。

--- src/container.js
+++ src/container.js
@@ -1195,9 +1195,13 @@ Neo.submit = function (board, blob, thumbnail, thumbnail2) {
 
     var errorMessage = null;
     if (request.status / 100 != 2) {
-	errorMessage = "Error " + request.status + "\n" 
-	+ request.responseURL + "\n"
-                   + Neo.translate("投稿に失敗。時間を置いて再度投稿してみてください。");
+    if (request.status == 403) {
+        errorMessage = "403 forbidden\n";
+        } else if (request.status == 404) {
+            errorMessage = "404 not found\n";
+        }
+        errorMessage = errorMessage + request.responseURL + "\n" +
+        Neo.translate("投稿に失敗。時間を置いて再度投稿してみてください。");
     } else if (request.response.match(/^error\n/m)) {
       errorMessage = request.response.replace(/^error\n/m, '');
     } else {
@@ -1232,10 +1236,13 @@ Neo.submit = function (board, blob, thumbnail, thumbnail2) {
     Neo.submitButton.enable();
   };
   request.ontimeout = function (e) {
+    var errorMessage = null;
+    errorMessage = Neo.translate("投稿に失敗。時間を置いて再度投稿してみてください。");
+    alert(errorMessage);
     console.log("timeout");
     Neo.submitButton.enable();
   };
-
+  request.setRequestHeader("X-Requested-With", "PaintBBS");
   request.send(body);
 };
 
