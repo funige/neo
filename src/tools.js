ca2afccecf14ba601949d4b3ff01a8c85d40608e
satopian <satopian@mia.eek.jp>
2026-09-08 17:25:49 +0900

    一連の問題の根本的解決として、キャンバスサイズよりも選択範囲が大きな時はキャンバスサイズを基準にして計算する。

--- src/tools.js
+++ src/tools.js
@@ -1176,6 +1176,11 @@ Neo.EffectToolBase = class extends Neo.ToolBase {
     this.endX = Math.floor(this.endX);
     this.endY = Math.floor(this.endY);
 
+    if (this.startX >= oe.canvasWidth) this.startX = oe.canvasWidth - 1;
+    if (this.endX >= oe.canvasWidth) this.endX = oe.canvasWidth - 1;
+    if (this.startY >= oe.canvasHeight) this.startY = oe.canvasHeight - 1;
+    if (this.endY >= oe.canvasHeight) this.endY = oe.canvasHeight - 1;
+
     var x = this.startX < this.endX ? this.startX : this.endX;
     var y = this.startY < this.endY ? this.startY : this.endY;
     var width = Math.abs(this.startX - this.endX) + 1;
@@ -1407,10 +1412,15 @@ Neo.TurnTool = class extends Neo.EffectToolBase {
     this.endX = Math.floor(this.endX);
     this.endY = Math.floor(this.endY);
 
+    if (this.startX >= oe.canvasWidth) this.startX = oe.canvasWidth - 1;
+    if (this.endX >= oe.canvasWidth) this.endX = oe.canvasWidth - 1;
+    if (this.startY >= oe.canvasHeight) this.startY = oe.canvasHeight - 1;
+    if (this.endY >= oe.canvasHeight) this.endY = oe.canvasHeight - 1;
+
     var x = this.startX < this.endX ? this.startX : this.endX;
     var y = this.startY < this.endY ? this.startY : this.endY;
-    var width = Math.abs(this.startX - this.endX);
-    var height = Math.abs(this.startY - this.endY);
+    var width = Math.abs(this.startX - this.endX) + 1;
+    var height = Math.abs(this.startY - this.endY) + 1;
 
     if (width > 0 && height > 0) {
       oe._pushUndo();
